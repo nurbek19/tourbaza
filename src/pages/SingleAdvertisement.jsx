16ae61e15874ab4c6708bf4e7ee1806e1c461742
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-04-26 23:52:52 +0600

    add changes

--- src/pages/SingleAdvertisement.jsx
+++ src/pages/SingleAdvertisement.jsx
@@ -16,76 +16,14 @@ import { BottomDrawer } from './BottomDrawer';
 import { ExpandableText } from './ExpandableText';
 
 const SingleAdvertisement = ({ item, lang, onBackHandler, hideButton }) => {
-    // const [show, setShow] = useState(false);
-    // const [showText, setShowText] = useState(false);
     const [selected, setSelected] = useState();
-    // const [searchParams] = useSearchParams();
     const [name, setName] = useState('');
     const [houses, setHouses] = useState([]);
-    // const byLink = searchParams.get('bylink');
     const [phone, setPhone] = useState('');
     const [open, setOpen] = useState(false);
     const [amount, setAmount] = useState(1);
 
-    // const copyHandler = () => {
-    //     navigator.clipboard.writeText(item.phone);
-    //     setShowText(true);
-    // }
-
-    // const bookedDays = useMemo(() => {
-    //     if (!item.books) {
-    //         return [];
-    //     }
-
-    //     if (houses.length) {
-    //         const housesBookedDays = houses.reduce((acc, value) => {
-    //             const booksByHouseNumber = item.books[value].map((entity) => entity.book_date);
-    //             acc.push(...booksByHouseNumber);
-
-    //             return acc;
-    //         }, []);
-
-    //         const setFromArr = new Set(housesBookedDays);
-
-    //         return Array.from(setFromArr).map((d) => new Date(d));
-    //     }
-
-
-    // const commonDates = Object.values(item.books).map((arr) => (arr.map((el) => el.book_date)));
-
-    // if (commonDates.length === 0) {
-    //     return [];
-    // }
-
-    // return commonDates.reduce((acc, arr) => acc.filter(el => arr.includes(el))).map(date => new Date(date));
-
-    // }, [item.books, houses]);
-
-
-    // const housesList = useMemo(() => {
-    //     if (!item.books) {
-    //         return [];
-    //     }
-
-    //     if (selected.length) {
-    //         const arr = [];
-    //         const selectedDates = selected.map((date) => format(date, 'MM/dd/yyyy'));
-
-    //         Object.keys(item.books).forEach((key) => {
-    //             const disabled = selectedDates.some((d) => item.books[key].map((obj) => (obj.book_date)).includes(d));
-
-    //             arr.push({ number: key, disabled });
-    //         });
-
-    //         return arr;
-    //     }
-
-    //     return Object.keys(item.books).map((v) => ({ number: v, disabled: false }));
-    // }, [item.books, selected])
-
     const onSendData = () => {
-        // const selectedDates = selected.map((date) => format(date, 'MM/dd/yyyy'));
-
         const books = houses.reduce((acc, value) => {
             acc[value] = format(selected, 'MM/dd/yyyy');
 
@@ -161,7 +99,7 @@ const SingleAdvertisement = ({ item, lang, onBackHandler, hideButton }) => {
 
     return (
         <div className='search-container'>
-            {!hideButton && (<div className="back-button" onClick={onBackHandler}>« {DICTIONARY[lang].back}</div>)}
+            <div className="back-button" onClick={onBackHandler}>« {DICTIONARY[lang].back}</div>
             <div className={clsx('single-result-card', { 'card-padding': selected })}>
                 <div className="">
                     <div className="single-card">
@@ -172,33 +110,10 @@ const SingleAdvertisement = ({ item, lang, onBackHandler, hideButton }) => {
                             <p className="bold-title">
                                 {item.name}
                             </p>
-                            {/* {!byLink && <p><span>{DICTIONARY[lang].city}:</span> {item.city}</p>} */}
-                            {/* {!byLink && <p className='address-link-wrapper'><span>{DICTIONARY[lang].address}: </span>
-                                <a className='address-link' href={`https://2gis.kg/search/${encodeURIComponent(item.city + ' ' + item.address)}`} target='_blank'>
-                                    {item.address}
-                                    <img src={gis} alt="2 gis icon" />
-                                </a></p>} */}
-                            {/* <p><span>{DICTIONARY[lang].roomCount}:</span> {item.count}</p> */}
-
-                            {/* {(!byLink && !item.mbank_link) || (!byLink && !item.finik_account_id) && (
-                                <div className="card-prices single-card-prices">
-                                    {Object.entries(item.price).map(([key, value]) => {
-                                        if (!value) {
-                                            return null;
-                                        }
-
-                                        return (
-                                            <div key={key}>{DICTIONARY[lang][key]} <br /> {value}</div>
-                                        );
-                                    })}
-                                </div>
-                            )} */}
 
                             <div className='book-calendar'>
                                 <p>{DICTIONARY[lang].bookLabel}:</p>
                                 <div className="field-wrapper">
-                                    {/* <label htmlFor="name" className="field-label">Название тура</label> */}
-
                                     <input type="text" id="name" placeholder='Выбрать дату' value={inputValue} className="text-field" readOnly={true} onFocus={() => setOpen(true)} />
                                 </div>
                             </div>
@@ -270,37 +185,6 @@ const SingleAdvertisement = ({ item, lang, onBackHandler, hideButton }) => {
                         </div>
                     </div>
                 </div>
-
-                {/* {((housesList.length !== 1 && item.mbank_link) || (housesList.length !== 1 && item.finik_account_id)) && (
-                    <div className='houses-container'>
-                        <p>Выберите номер дома для бронирования:</p>
-                        <div className='houses-list'>
-                            {housesList.map((obj) => (
-                                <HouseItem key={obj.number} number={obj.number} disabled={obj.disabled} setHouses={setHouses} />
-                            ))}
-                        </div>
-                    </div>
-                )} */}
-
-                {/* <div className={clsx('field-wrapper hide-name-field', { 'show-name-field': selected.length && houses.length })}>
-                    <label htmlFor="name" className="field-label">Введите ваше имя</label>
-
-                    <input type="text" id="name" className="text-field" value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-
-                <div className={clsx('field-wrapper phone-field', { 'show-number': selected.length })}>
-                    <label htmlFor="phone" className="field-label">{DICTIONARY[lang].bookPhone}</label>
-
-                    <input
-                        type="tel"
-                        pattern="[0-9]*"
-                        noValidate id="phone"
-                        className="text-field"
-                        placeholder="0555 555 555"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                        maxLength={10} />
-                </div> */}
 
                 <button onClick={onSendData}>btn</button>
             </div>
